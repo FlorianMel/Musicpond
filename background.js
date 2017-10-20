f1a0e4a93239507262f2c1d0804d01ac629c9fc2
FlorianMel <32706798+FlorianMel@users.noreply.github.com>
2017-10-20 15:15:35 +0200

    MAJ final

--- background.js
+++ background.js
@@ -38,8 +38,208 @@ $(document).ready(function(){
         redimensionnement();
     });
 
-});
 
-$('.ui.labeled.icon.sidebar')
-  .sidebar('toggle')
-;
+/*    $('.ui.labeled.icon.sidebar')
+        .sidebar('toggle')
+    ;*/
+
+
+
+    $.getJSON("https://api.airtable.com/v0/appF1X1dfHOpDSVv2/Musique?api_key=keyXnk3EBClHYcj9b",
+     function(data) {
+         $.each(data.records, function(key,val) {
+             if(val.fields.ashtags == 'Rap') {
+                $("#musiques_rap").append(val.fields.URL);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.ashtags == 'Rock') {
+                $("#musiques_rock").append(val.fields.URL);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.ashtags == 'Pop') {
+                $("#musiques_pop").append(val.fields.URL);
+             }
+
+         });
+
+        //. TOP 1
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Another One Bites The Dust') {
+                $("#another_one_bites_the_dust_url").append(val.fields.URL_2);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Another One Bites The Dust') {
+                $("#another_one_bites_the_dust_titre").append(val.fields.Titre);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Another One Bites The Dust') {
+                $("#another_one_bites_the_dust_auteur").append(val.fields.Auteur);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Another One Bites The Dust') {
+                $("#another_one_bites_the_dust_pochette").append(val.fields.pochette);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Another One Bites The Dust') {
+                $("#another_one_bites_the_dust_album").append(val.fields.album);
+             }
+
+         });
+
+        //. TOP 2
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Aquatrip') {
+                $("#aquatrip_url").append(val.fields.URL_2);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Aquatrip') {
+                $("#aquatrip_titre").append(val.fields.Titre);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Aquatrip') {
+                $("#aquatrip_auteur").append(val.fields.Auteur);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Aquatrip') {
+                $("#aquatrip_pochette").append(val.fields.pochette);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Aquatrip') {
+                $("#aquatrip_album").append(val.fields.album);
+             }
+
+         });
+
+        //. TOP 3
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'The Trooper') {
+                $("#the_trooper_url").append(val.fields.URL_2);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'The Trooper') {
+                $("#the_trooper_titre").append(val.fields.Titre);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'The Trooper') {
+                $("#the_trooper_auteur").append(val.fields.Auteur);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'The Trooper') {
+                $("#the_trooper_pochette").append(val.fields.pochette);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'The Trooper') {
+                $("#the_trooper_album").append(val.fields.album);
+             }
+
+         });
+
+        //. TOP 4
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Thrift Shop') {
+                $("#thrift_shop_url").append(val.fields.URL_2);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Thrift Shop') {
+                $("#thrift_shop_titre").append(val.fields.Titre);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Thrift Shop') {
+                $("#thrift_shop_auteur").append(val.fields.Auteur);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Thrift Shop') {
+                $("#thrift_shop_pochette").append(val.fields.pochette);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'Thrift Shop') {
+                $("#thrift_shop_album").append(val.fields.album);
+             }
+
+         });
+
+        //. TOP 5
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'In Da Club (Int.l Version)') {
+                $("#in_da_club_url").append(val.fields.URL_2);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'In Da Club (Int.l Version)') {
+                $("#in_da_club_titre").append(val.fields.Titre);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'In Da Club (Int.l Version)') {
+                $("#in_da_club_auteur").append(val.fields.Auteur);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'In Da Club (Int.l Version)') {
+                $("#in_da_club_pochette").append(val.fields.pochette);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Titre == 'In Da Club (Int.l Version)') {
+                $("#in_da_club_album").append(val.fields.album);
+             }
+
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Votes == '56') {
+                $("#top_one").append(val.fields.URL);
+             }
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Votes == '41') {
+                $("#top_two").append(val.fields.URL);
+             }
+         });
+        $.each(data.records, function(key,val) {
+             if(val.fields.Votes == '32') {
+                $("#top_three").append(val.fields.URL);
+             }
+         });
+       }
+     );
+});
